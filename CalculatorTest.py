2015e6003af0afe80e7c4b65c231b1abf7bacf97
HassanB1 <hassanb1@aetna.com>
2020-11-06 13:11:40 -0500

    calculator requirements

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -3,11 +3,12 @@ import Calculator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_addition(self):
-        assert 4 == Calculator.add(2,2)
-    def test_subtraction(self):
-        assert 2 == Calculator.subtract(4,2)
 
+    def test_addition(self):
+        assert 4 == Calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == Calculator.subtract(4, 2)
 
 
 if __name__ == '__main__':
