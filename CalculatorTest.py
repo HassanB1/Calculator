a3f85153cb48faabda8abf652c7f2e2577c97e35
HassanB1 <hassanb1@aetna.com>
2020-11-06 12:16:16 -0500

    calculator test

---
+++ CalculatorTest.py
@@ -0,0 +1,14 @@
+import unittest
+import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+    def test_addition(self):
+        assert 4 == Calculator.add(2,2)
+    def test_subtraction(self):
+        assert 2 == Calculator.subtract(4,2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
